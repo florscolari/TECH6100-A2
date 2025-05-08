2a5937315aa63818257e5187b61724726e1df0bd
Florencia Scolari <florscolari0@gmail.com>
2025-05-08 12:16:35 +0800

    Book class & 10 attributes with their datatypes

--- TECH6100A2.py
+++ TECH6100A2.py
@@ -1,1 +1,20 @@
-#1st comment
+#todo: Create Book class
+class Book:
+    def __init__(self, title, author, price, quantity, book_type, isbn, publication_date, language, publisher,
+                 book_format):
+        self.title: str = title
+        self.author: str = author
+        self.price: float = price
+        self.quantity: int = quantity
+        self.book_type: str = book_type
+        self.isbn: str = isbn
+        self.publication_date: str = publication_date
+        self.publisher: str = publisher
+        self.language: str = language
+        self.book_format: str = book_format
+#todo: Book Attributes x10
+
+#todo: Use Enum class for list of constant values (genre, format, language)
+
+#todo: constructor init & repr
+
