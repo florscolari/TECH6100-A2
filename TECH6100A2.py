62e0d3b0b2fd3e2106b30d7ad32b58e13c20c533
Florencia Scolari <florscolari0@gmail.com>
2025-05-08 14:48:40 +0800

    ADDED Book Class Getters & Setters, All attributes are private now

--- TECH6100A2.py
+++ TECH6100A2.py
@@ -35,50 +35,112 @@ class Language(StrEnum):
 #todo: Language setters
 
 class Book:
+
     def __init__(self, title, author, price, quantity, book_genre: BookGenre, isbn, publication_year,
                  language: Language,
                  publisher,
                  book_format: BookFormat):
-        self.title: str = title
-        self.author: str = author
-        self.price: float = price
-        self.quantity: int = quantity
-        self.book_genre: BookGenre = book_genre
-        self.isbn: str = isbn
-        self.publication_year: int = publication_year
-        self.publisher: str = publisher
-        self.language: Language = language
-        self.book_format: BookFormat = book_format
+        self.__title: str = title
+        self.__author: str = author
+        self.__price: float = price
+        self.__quantity: int = quantity
+        self.__book_genre: BookGenre = book_genre
+        self.__isbn: str = isbn
+        self.__publication_year: int = publication_year
+        self.__publisher: str = publisher
+        self.__language: Language = language
+        self.__book_format: BookFormat = book_format
 
     #To display data from a class object to users
     def __str__(self):
        return (f"------\n"
-               f"Title: {self.title.title()} - Author: {self.author.title()}\n"
-               f"Genre: {self.book_genre}\n"
-               f"ISBN: {self.isbn}, Published on {self.publication_year} by {self.publisher}\n"
-               f"Format: {self.book_format} - Language: {self.language}\nPrice: ${self.price}\nAvailable:"
-               f" {self.quantity}")
+               f"Title: {self.__title.title()} - Author: {self.__author.title()}\n"
+               f"Genre: {self.__book_genre}\n"
+               f"ISBN: {self.__isbn}, Published on {self.__publication_year} by {self.__publisher}\n"
+               f"Format: {self.__book_format} - Language: {self.__language}\nPrice: ${self.__price}\nAvailable:"
+               f" {self.__quantity}")
 
     #To display data from a class object to programmers
     def __repr__(self):
         return (f"------\n"
-                f"Title: {self.title}.title(): {type(self.title)} - Author: {self.author}.title():"
-                f" {type(self.author)}\n"
-                f"Genre: {self.book_genre}: {type(self.book_genre)}\n"
-                f"ISBN: {self.isbn}: {type(self.isbn)}, Published on {self.publication_year}: {type(self.publication_year)} by {
-                self.publisher}: {type(self.publisher)}\n"
-                f"Format: {self.book_format}: {type(self.book_format)} - Language: {self.language}: "
-                f"{type(self.language)}\nPrice: ${
-                self.price}: {type(self.price)}\nAvailable:"
-                f" {self.quantity}: {type(self.quantity)}")
+                f"Title: {self.__title}.title(): {type(self.__title)} - Author: {self.__author}.title():"
+                f" {type(self.__author)}\n"
+                f"Genre: {self.__book_genre}: {type(self.__book_genre)}\n"
+                f"ISBN: {self.__isbn}: {type(self.__isbn)}, Published on {self.__publication_year}: {type(self.__publication_year)} by {
+                self.__publisher}: {type(self.__publisher)}\n"
+                f"Format: {self.__book_format}: {type(self.__book_format)} - Language: {self.__language}: "
+                f"{type(self.__language)}\nPrice: ${
+                self.__price}: {type(self.__price)}\nAvailable:"
+                f" {self.__quantity}: {type(self.__quantity)}")
 
 
-#todo: Book getters
+    #Book Getters
+    def getTitle(self):
+        return self.__title
 
-#todo: Book setters
+    def getAuthor(self):
+        return self.__author
+
+    def getPrice(self):
+        return self.__price
+
+    def getQuantity(self):
+        return self.__quantity
+
+    def getBookGenre(self):
+        return self.__book_genre
+
+    def getIsbn(self):
+        return self.__isbn
+
+    def getPublicationYear(self):
+        return self.__publication_year
+
+    def getPublisher(self):
+        return self.__publisher
+
+    def getLanguage(self):
+        return self.__language
+
+    def getBookFormat(self):
+        return self.__book_format
+
+    # Book Setters
+    def setTitle(self, value):
+        self.__title = value
+
+    def setAuthor(self, value):
+        self.__author = value
+
+    def setPrice(self, value):
+        self.__price = value
+
+    def setQuantity(self, value):
+        self.__quantity = value
+
+    """Set Book Genre is handled from BookGenre class"""
+
+    def setIsbn(self, value):
+        self.__isbn = value
+
+    def setPublicationYear(self, value):
+        self.__publication_year = value
+
+    def setPublisher(self, value):
+        self.__publisher = value
+
+    """Set Language is handled from Language class"""
+
+    """Set Book Format is handled from BookFormat class"""
+
 
 
 book1 = (Book("title", "John Doe", 25, 2, BookGenre.SCIFI, "1234ases", 1987, Language.ENGLISH, "Planet Editions",
          BookFormat.PAPERBACK) )
 book2 = Book("title2", "author2", 45, 1, BookGenre.FICTION, "qwert1", 1988, Language.ENGLISH, "Planeta",
              BookFormat.PAPERBACK)
+
+print(book1.getTitle())
+print(book1)
+book1.setAuthor("new guy")
+print(book1)
